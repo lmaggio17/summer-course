f6cd37dd3bae69cad0bee7095106a222b50fc59c
Lauren Maggio <laurenmaggio57@gmail.com>
2026-07-10 15:34:15 -0400

    Updates

--- Python/Lesson 01 - Python Basics/python-notes.py
+++ Python/Lesson 01 - Python Basics/python-notes.py
@@ -1,11 +1,22 @@
-def pythagorean_theorem(a, b):
-    c_squared = a**2 + b**2
-    c = c_squared ** 0.5
-    return c
+small_size= int(input("How big is the small pizza? "))
+xsmall= int(input("How many small pizzas are you buying? "))
+small_price= float(input("How much does the small pizza cost? "))
 
+large_size= int(input("How big is the large pizza? "))
+xlarge= int(input("How many large pizzas are you buying? "))
+large_price= float(input("How much does the large pizza cost? "))
 
-a = float(input("Enter value for a: "))
-b = float(input("Enter value for b: "))
+small_area= (3.14*(small_size/2)**2.0)* xsmall
+large_area= (3.14*(large_size/2)**2.0)* xlarge
 
-c = pythagorean_theorem(a, b)
-print(f"For a = {a} and b = {b}, c = {c:.2f}")
+print("Your small pizza is", small_area)
+print("Your large pizza is", large_area)
+
+print("The small pizza costs", small_area/small_price, "per square inch")
+print("The large pizza costs", large_area/large_price, "per square inch")
+
+if small_area < large_area:
+    print("The large pizza is a better deal")
+else:
+    print("The small pizza is a better deal")
+    
